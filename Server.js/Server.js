4ea8c0ff1ef0cb7e108d7987ae48aa63c960d388
Madow-s <Madeleinesarr60@gmail.com>
2025-01-18 16:13:52 +0100

    Update Server.js

--- Server.js/Server.js
+++ Server.js/Server.js
@@ -12,23 +12,33 @@ const madoiciaussimets=mysql.createConnection({
 host :'localhost',
 user:'root',
 password:'',
-database:'mado mets ici la base de donnée'
+database:'projetnodejs'
 });
+
+
+
+
 //connection entre la base de donnée
-madometsicilabasededonnée.connect((err)=>{
+projetnodejs.connect((err)=>{
 if(err){
     console.error('erreur de  connexion avec la base de donnée'+err.stock)
     return;
 }
-console.log('connexion reussie'+db.threadId);
+console.log('connexion reussie'+projetnodejs.threadId);
 });
+
+
+
 //Middlewave
 app.use(bp.json());
 app.use(express.stalic('public'));
+
+
 //route pour ajouter un utilisateur 
 app.use('/api/user',(req,res)=>{
     const{prenom,nom,login,password,profile}=req.body;
     const sql="Insert into user (prenom,nom,login,password,profile) values(?,?,?,?,?)";
-    db.querry(sql,[prenom,nom,login,password,profile],(error,result)=>{})
+    projetnodejs.querry(sql,[prenom,nom,login,password,profile],(error,result)=>{})
 });
 //Madopartout ou tu vois ton nom ou tu vois db mets s'il te plait le nom de la base de donnée ok 
+//ok c'est fait
