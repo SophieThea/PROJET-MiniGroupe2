5d82232e97f7ed9f6498c16968c9d6d09f22767a
Madow-s <Madeleinesarr60@gmail.com>
2025-01-21 17:00:09 +0100

    Update scriptDossier.js

--- public/scriptDossier.js
+++ public/scriptDossier.js
@@ -1,5 +1,17 @@
-document.getElementById('patientform').addEventListener('submit', async (e) => {
+document.getElementById('formDossier').addEventListener('submit', async (e) => {
     e.preventDefault(); // Empêche le rechargement de la page
 
+    const Date_de_creation = document.getElementById('dcreation').value;
 
-};
+    await fetch('http://localhost:3000/api/dossiers', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ Date_de_creation })
+      });
+
+      alert('Dossier ajouté avec succès !');
+      document.getElementById('formDossier').reset();
+  
+
+
+});
